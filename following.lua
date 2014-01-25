34665de51a2fdb923aec680100a6516e5f144baf
Matthew Batchelder <borkweb@gmail.com>
2014-01-25 12:06:23 -0500

    alphabetize

--- following.lua
+++ following.lua
@@ -16,22 +16,41 @@ function following_response()
 	if true == follow_helper then
 		say = matches[2]
 
-		if "birdie" == say then send( "ride birdie" )
+		if "altar" == say then
+			send( "place claws on altar" )
+			send( "place hands on altar" )
+			send( "place paws on altar" )
+			send( "say death" )
+		elseif "birdie" == say then send( "ride birdie" )
+		elseif "book" == say then send( "read book" )
+		elseif "cave" == say then send( "cave" )
 		elseif "deny humanity" == say then
 			send( "say deny humanity" )
 			tempTimer( 2, [[
 				send( "stand" )
 			]])
-		elseif "pay guard" == say then
-			send( "pay guard" )
-		elseif "move sheets" == say then
-			send( "move sheets" )
-			send( "lay on bed" )
+		elseif "elevator" == say then send( "elevator" )
+		elseif "enter gate" == say then send( "enter gate" )
+		elseif "enter mirror" == say then send( "enter mirror" )
+		elseif "enter portal" == say then send( "enter portal" )
+		elseif "enter solace" == say then send( "enter solace" )
+		elseif "daisy" == say then
+			send( "pinch daisy" )
+		elseif "door" == say then send( "open door" )
+		elseif "gate" == say then send( "enter gate" )
+		elseif "grab m" == say then send( "grab m" )
+		elseif "leave" == say then send( "leave" )
+		elseif "leave solace" == say then send( "leave solace" )
 		elseif "[automated] lick staff and give back please" == say then
 			send( "lick staff" )
 			send( "give crystallized lysergic staff to " .. follow_who )
-		elseif "door" == say then send( "open door" )
-		elseif "elevator" == say then send( "elevator" )
+		elseif "mirror" == say then send( "enter mirror" )
+		elseif "move sheets" == say then
+			send( "move sheets" )
+			send( "lay on bed" )
+		elseif "orb" == say then send( "use orb" )
+		elseif "pay guard" == say then
+			send( "pay guard" )
 		elseif "portal" == say then send( "enter portal" )
 		elseif "push 1" == say then send( "push 13.1" )
 		elseif "push 2" == say then send( "push 13.2" )
@@ -42,40 +61,22 @@ function following_response()
 		elseif "push 7" == say then send( "push 13.7" )
 		elseif "push 8" == say then send( "push 13.8" )
 		elseif "push 9" == say then send( "push 13.9" )
-		elseif "enter portal" == say then send( "enter portal" )
-		elseif "mirror" == say then send( "enter mirror" )
-		elseif "enter mirror" == say then send( "enter mirror" )
-		elseif "orb" == say then send( "use orb" )
-		elseif "use orb" == say then send( "use orb" )
-		elseif "solace" == say then send( "enter solace" )
-		elseif "enter solace" == say then send( "enter solace" )
-		elseif "leave solace" == say then send( "leave solace" )
-		elseif "leave" == say then send( "leave" )
-		elseif "tv" == say then send( "watch tv" )
-		elseif "watch tv" == say then send( "watch tv" )
-		elseif "gate" == say then send( "enter gate" )
-		elseif "enter gate" == say then send( "enter gate" )
-		elseif "book" == say then send( "read book" )
 		elseif "read book" == say then send( "read book" )
 		elseif "rest" == say then send( "rest" )
-		elseif "stand" == say then send( "stand" )
-		elseif "xp" == say then send( "gs " .. format_int( xp ) )
-		elseif "cave" == say then send( "cave" )
-		elseif "scarecrow" == say then send( "touch scarecrow" )
-		elseif "wish on star" == say then send( "wish on star" )
-		elseif "altar" == say then
-			send( "place claws on altar" )
-			send( "place hands on altar" )
-			send( "place paws on altar" )
-			send( "say death" )
 		elseif "sams" == say then
 			expandAlias( "#go heart" )
 			expandAlias( "#go spiced" )
+		elseif "scarecrow" == say then send( "touch scarecrow" )
+		elseif "solace" == say then send( "enter solace" )
+		elseif "stand" == say then send( "stand" )
+		elseif "tv" == say then send( "watch tv" )
+		elseif "use orb" == say then send( "use orb" )
+		elseif "watch tv" == say then send( "watch tv" )
+		elseif "wish on star" == say then send( "wish on star" )
+		elseif "xp" == say then send( "gs " .. format_int( xp ) )
 		elseif "zarinia" == say then
 			expandAlias( "#go heart" )
 			expandAlias( "#go zarinia" )
-		elseif "daisy" == say then
-			send( "pinch daisy" )
 		end
 	end
 end
